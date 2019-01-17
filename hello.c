703fba0bb08c8da3303c5116f8a7b56fdc322f8b
Zachary Statema <zstate15@gmail.com>
2019-01-17 09:54:07 -0600

    Create hello.c
    Added Hello.c file

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Zach Statema
+ * Date: 2018/15/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zach Statema\n");
+
+  return 0;
+}
